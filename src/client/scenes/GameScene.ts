0b103a2577d77d3cf2d9f19bd8c256ce5de4d0c6
deirdre <deirdre.loyall@gmail.com>
2023-02-28 13:09:11 -0500

    players now displaying on client on room enter

--- src/client/scenes/GameScene.ts
+++ src/client/scenes/GameScene.ts
@@ -1,13 +1,19 @@
 import Phaser from 'phaser';
 import { Client, Room } from 'colyseus.js';
+import Player from '../entities/Player';
 
 export class GameScene extends Phaser.Scene {
   preload() {
-    // preload scene
+    this.load.image('background', '../../../assets/background.png');
+    this.load.image('floor', '../../../assets/floor.png');
+    this.load.spritesheet('player', '../../../assets/hero_stand_run.png',
+                          {frameWidth: 50, frameHeight: 50});
   }
 
   client = new Client("ws://localhost:3000");
   room: Room;
+
+  playerEntities: {[sessionId: string]: any} = {};
 
   async create() {
     console.log('Joining game!');
@@ -17,6 +23,21 @@ export class GameScene extends Phaser.Scene {
       console.log('Joined successfully! :)')
     } catch (err) {
       console.log(err);
+    }
+
+    this.room.state.players.onAdd = (player, sessionId) => {
+      const entity = new Player(this, player.x, player.y);
+
+      this.playerEntities[sessionId] = entity;
+    }
+
+    this.room.state.players.onRemove = (player, sessionId) => {
+      const entity = this.playerEntities[sessionId];
+      if (entity) {
+        entity.destroy();
+      }
+
+      delete this.playerEntities[sessionId];
     }
   }
 
--- src/server/GameRoom.ts
+++ src/server/GameRoom.ts
@@ -1,17 +1,33 @@
 import http from "http";
 import { Room, Client } from "colyseus";
+import { Player, GameRoomState } from './GameRoomState';
 
-export class GameRoom extends Room {
+export class GameRoom extends Room<GameRoomState> {
     // When room is initialized
-    onCreate (options: any) { }
+    onCreate (options: any) {
+        this.setState(new GameRoomState());
+    }
 
     // When client successfully join the room
     onJoin (client: Client, options: any, auth: any) {
-        console.log(client.id, 'has joined!');
+        console.log(client.sessionId, 'has joined!');
+
+        const player = new Player();
+
+        player.x = 400;
+        player.y = 300;
+
+        //assigns player to state
+        this.state.players.set(client.sessionId, player);
+
      }
 
     // When a client leaves the room
-    onLeave (client: Client, consented: boolean) { }
+    onLeave (client: Client, consented: boolean) {
+        console.log(client.sessionId, 'left!');
+
+        this.state.players.delete(client.sessionId);
+    }
 
     // Cleanup callback, called after there are no more clients in the room. (see `autoDispose`)
     onDispose () { }
