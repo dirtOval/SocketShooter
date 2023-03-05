808efc022d9e0ee3e5be02fe5d7cb2b1e69641df
deirdre <deirdre.loyall@gmail.com>
2023-03-05 16:25:08 -0500

    set up static hosting for the game

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,8 +1,16 @@
 import { Server } from "colyseus"
 import { GameRoom } from './GameRoom';
+import {createServer} from 'http';
+import express from 'express';
+const path = require('path');
+const app = express();
 const port = parseInt(process.env.PORT) || 3000
 
-const gameServer = new Server()
+const gameServer = new Server({
+  server: createServer(app)
+});
+app.use(express.static('dist'));
+app.use(express.json());
 
 gameServer.define('gameroom', GameRoom);
 
